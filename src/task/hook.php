144842740f42d07d4403fabc1e1e9a55f0a5fe21
Mikhail Bredikhin <mikha.dev@gmail.com>
2022-08-16 16:00:13 +0200

    first commit

---
+++ src/task/hook.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Deployer;
+
+task('hook:start', function() {})->hidden();
+task('hook:build', function() {})->hidden();
+task('hook:ready', function() {})->hidden();
+task('hook:done', function() {})->hidden();
+task('hook:rollback', function() {})->hidden();
